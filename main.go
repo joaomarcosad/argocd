d41ea26fd50f8aa2552b8101aa1a4b677d141b1d
João Marcos A. Dantas <joaomarcos.ad@gmail.com>
2023-09-06 22:15:19 -0300

    Mudança main.go

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "net/http"
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>LAB ARGOCD</h1>"))
+		w.Write([]byte("<h1>ARGOCD LAB - TESTE</h1>"))
 	})
 	http.ListenAndServe(":8080", nil)
 
